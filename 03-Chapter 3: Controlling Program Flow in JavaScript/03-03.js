4f01c2c32b1ecc6d5e05ba64872ffd9d4aba7e68
leah-clydebankmedia <leah@clydebankmedia.com>
2024-07-11 14:26:36 -0400

    Create 03-03.js

---
+++ 03-Chapter 3: Controlling Program Flow in JavaScript/03-03.js
@@ -0,0 +1,15 @@
+let a = 5
+let b = 10
+let c = 100
+
+if (b < c) {
+	console.log(`${b} is less than ${c}`)
+}
+
+if (c > b) {
+	console.log(`${c} is greater than ${b}`)
+}
+
+if (a <= 5) {
+	console.log(`${a} is less than or equal to 5`)
+}

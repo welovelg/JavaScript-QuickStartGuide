0fdcce38cd52b28a9e72010cdd8daae02e513f82
leah-clydebankmedia <leah@clydebankmedia.com>
2024-07-11 14:27:11 -0400

    Create 03-05.js

---
+++ 03-Chapter 3: Controlling Program Flow in JavaScript/03-05.js
@@ -0,0 +1,10 @@
+let a = 1
+let b = 2
+
+if (a == 1 || b == 3) {
+	console.log("At least one of these conditions is true.")
+}
+
+if (a == 1 || b == 2 || a == b) {
+	console.log("At least one of the three conditions is true.")
+}

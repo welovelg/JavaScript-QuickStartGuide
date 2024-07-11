5f1ea171aebfdb2486c7a8f12f75d93d857b75d9
leah-clydebankmedia <leah@clydebankmedia.com>
2024-07-11 14:26:54 -0400

    Create 03-04.js

---
+++ 03-Chapter 3: Controlling Program Flow in JavaScript/03-04.js
@@ -0,0 +1,10 @@
+let a = 1
+let b = 2
+
+if (a == 1 && b == 2) {
+	console.log("a is 1 and b is 2")
+}
+
+if (a == 1 && b == 2 && a != b) {
+	console.log("a is 1, b is 2, and they are not equal")
+}

18809ac16c258bb2a7a7305a7dc0663cd8782310
leah-clydebankmedia <leah@clydebankmedia.com>
2024-07-11 14:28:07 -0400

    Create 03-07.js

---
+++ 03-Chapter 3: Controlling Program Flow in JavaScript/03-07.js
@@ -0,0 +1,15 @@
+let color = "blue"
+
+switch (color) {
+	case "red":
+		console.log("The color is red.")
+		break
+	case "orange":
+		console.log("The color is orange.")
+		break
+	case "purple":
+		console.log("The color is purple.")
+		break
+	default:
+		console.log("I'm not sure what color it is.")
+}

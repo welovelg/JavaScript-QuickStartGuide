4223c20c2a8b85804276f0307a3047111e58c9fc
leah-clydebankmedia <leah@clydebankmedia.com>
2024-07-11 14:53:17 -0400

    Create clicked-09.js

---
+++ 09-Chapter 9: Asynchronous JavaScript/clicked-09.js
@@ -0,0 +1,13 @@
+let theButton = document.getElementById("theButton");
+
+theButton.addEventListener("click", (function() {
+	// Initially set the clickCount to 0
+	let clickCount = 0
+	
+    return function() {
+    	// Increment the clickCount
+		clickCount += 1
+		// Update the button's textContent
+		this.textContent = `Clicked ${clickCount} times!`
+    }
+})())

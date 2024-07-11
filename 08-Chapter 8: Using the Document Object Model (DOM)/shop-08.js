252132fa9e9657365156ca92c825f701cffc4051
leah-clydebankmedia <leah@clydebankmedia.com>
2024-07-11 14:51:45 -0400

    Create shop-08.js

---
+++ 08-Chapter 8: Using the Document Object Model (DOM)/shop-08.js
@@ -0,0 +1,32 @@
+// Display a quick message in the console.
+console.log("ClydeBank Coffee Shop is now open!")
+
+// The inventory object
+let menu = {
+    inventory: {
+        "Regular Coffee": 3.00,
+        "Espresso": 3.50,
+        "Cappuccino": 4.00,
+        "Latte": 4.25
+    },
+    populate: function(container) {
+        for (let item in this.inventory) {
+            let price = this.inventory[item]
+            
+            // Create a new li element
+            let li = document.createElement("li")
+            
+            // Add text content to the li element
+            li.textContent = `${item} - $${price}`
+            
+            // Append the li element to the container
+            container.appendChild(li)
+        }
+    }
+}
+
+// Obtain reference to the menu list by ID
+let menuList = document.getElementById("coffee-menu")
+
+// Populate the menu
+menu.populate(menuList)

3efd58732b6016ef202e17184d196e2275d77979
leah-clydebankmedia <leah@clydebankmedia.com>
2024-10-02 17:03:32 -0400

    Update 04-03.js

--- 04-Chapter 4 - Functions/04-03.js
+++ 04-Chapter 4 - Functions/04-03.js
@@ -1,8 +1,8 @@
 greetingContainer = document.getElementById("greeting")
-			sayHello = document.getElementById("sayHello")
+sayHello = document.getElementById("sayHello")
 
 function greeting() {
-    greetingContainer.textContent = "Hello, World!"
+	greetingContainer.textContent = "Hello, World!"
 }
 
 sayHello.addEventListener("click", greeting())

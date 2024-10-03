19aa1bae3aa709d224134e6585f0f7f15e5a6d8d
leah-clydebankmedia <leah@clydebankmedia.com>
2024-10-03 15:54:22 -0400

    Update 09-03.js

--- 09-Chapter 9 - Asynchronous JavaScript/09-03.js
+++ 09-Chapter 9 - Asynchronous JavaScript/09-03.js
@@ -1,18 +1,18 @@
 let examplePromise = new Promise(function(resolve, reject) {
-		// First set success to false so that only a successful
-		// process will trigger a success
-		success = false
+	// First set success to false so that only a successful
+	// process will trigger a success
+	success = false
 
-    // Perform any long-running task
-    // If task successful, set success = true
+	// Perform any long-running task
+	// If task successful, set success = true
 
-    if (success) {
-        resolve("Success!")
-    } else {
-        reject("Failure!")
-    }
+	if (success) {
+		resolve("Success!")
+	} else {
+		reject("Failure!")
+	}
 });
 
 examplePromise.then(function(value) {
-    console.log(value)
+	console.log(value)
 })

c7a53e378a4387fa076a839a5d50822c8ea89cf3
leah-clydebankmedia <leah@clydebankmedia.com>
2024-10-03 15:51:27 -0400

    Update and rename clicked-09.js to clicked.js

--- 09-Chapter 9 - Asynchronous JavaScript/clicked.js
+++ 09-Chapter 9 - Asynchronous JavaScript/clicked.js
@@ -3,11 +3,11 @@ let theButton = document.getElementById("theButton");
 theButton.addEventListener("click", (function() {
 	// Initially set the clickCount to 0
 	let clickCount = 0
-	
-    return function() {
-    	// Increment the clickCount
+
+	return function() {
+	// Increment the clickCount
 		clickCount += 1
 		// Update the button's textContent
 		this.textContent = `Clicked ${clickCount} times!`
-    }
+	}
 })())
